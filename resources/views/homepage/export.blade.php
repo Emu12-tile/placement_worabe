580a299927f645ba7ef5bd7c47234170dec6b53f
Emebet Tilahun <emuaniley@gmail.com>
2023-04-18 00:23:19 -0700

    pdf

--- resources/views/homepage/export.blade.php
+++ resources/views/homepage/export.blade.php
@@ -211,7 +211,7 @@
 <script>
     var element = document.getElementById("element-to-print")
     html2pdf(element, {
-        margin: 9,
+        margin: 15,
         filename: 'Application form.pdf',
         image: {
             type: 'jpeg',
