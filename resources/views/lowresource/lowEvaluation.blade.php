336cb3ed59c7d3ffb314b2c8e1de2135a8d71827
Emebet Tilahun <emuaniley@gmail.com>
2023-05-28 21:51:40 +0300

    updated date

--- resources/views/lowresource/lowEvaluation.blade.php
+++ resources/views/lowresource/lowEvaluation.blade.php
@@ -636,19 +636,19 @@
     {{-- <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script> --}}
 
 
-    <script>
+    {{-- <script>
         $(document).ready(function() {
             var totalYear = 0;
             var totalMonth = 0;
             var totalDay = 0;
 
             $('.select').on('change', function() {
-                // Reset the totals when a new value is selected
+
                 totalYear = 0;
                 totalMonth = 0;
                 totalDay = 0;
 
-                // Iterate over each select element and calculate the sum
+
                 $('.select').each(function() {
                     var selectedValue = parseFloat($(this).val());
 
@@ -661,11 +661,8 @@
 
                     var startdate = new Date(row.startingDate);
                     var enddate = new Date(row.endingDate);
-                    // var experience = row.positionyouworked;
-                    // console.log(experience);
+
                     var evaluate = enddate - startdate;
-                    // var diffInDays = evaluate / (1000 * 60 * 60 * 24);
-                    //
 
 
 
@@ -675,9 +672,7 @@
                     var multiply = diffInDays * selectedValue;
 
 
-                    // var diffInDays = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24));
 
-                    // calculate the difference in years, months, and days
                     var years = Math.floor(multiply / 365);
                     var months = Math.floor((multiply % 365) / 30);
                     var days = (multiply % 365) % 30;
@@ -705,14 +700,155 @@
                         totalMonth = totalMonth - 12;
                     }
 
-                    // Update the sum for the current row
+
                     $(this).closest('tr').find('#add').text(all);
                 });
 
-                // Update the total sum
+                var total = totalYear + '-' + totalMonth + '-' + totalDay;
+                $('#total-year').text(total);
+            });
+        });
+    </script> --}}
+    <script>
+        $(document).ready(function() {
+            var totalYear = 0;
+            var totalMonth = 0;
+            var totalDay = 0;
+
+            $('.select').on('change', function() {
+                // Reset the totals when a new value is selected
+                totalYear = 0;
+                totalMonth = 0;
+                totalDay = 0;
+
+                // Iterate over each select element and calculate the sum
+                $('.select').each(function() {
+                    var selectedValue = parseFloat($(this).val());
+
+                    var data = $(this).closest('tr').data('id');
+                    var myData = {!! json_encode($forms) !!};
+
+                    var row = myData.find(function(obj) {
+                        return obj.id === data;
+                    });
+
+                    var startdate = new Date(row.startingDate);
+                    var enddate = new Date(row.endingDate);
+                    var date = new Date(row.startingDate);
+                    // console.log(startdate);
+                    // var unix = Math.floor(date.getTime() / 1000);
+                    const years = startdate.getFullYear();
+                    const months = startdate.getMonth() + 1;
+                    const days = startdate.getDate();
+                    const years2 = enddate.getFullYear();
+                    const months2 = enddate.getMonth() + 1;
+                    const days2 = enddate.getDate();
+                    let dayDifferenceb = (days2 - days);
+                    let monthDifferenceb = (months2 - months);
+                    let yearDifferenceb = (years2 - years);
+
+
+                    let dayDifference = (days2 - days) * selectedValue;
+                    let monthDifference = (months2 - months) * selectedValue;
+                    let yearDifference = (years2 - years) * selectedValue;
+
+                    if (selectedValue == 0.5) {
+                        if (yearDifferenceb % 2 != 0) {
+                            yearDifference = parseInt(yearDifference);
+                            console.log(yearDifference);
+                            // monthDifference+=6
+                            monthDifference = 6 + (monthDifference);
+                            //    console.log(monthDifference);
+                            if (monthDifference >= 12) {
+                                monthDifference = 0;
+                                yearDifference = yearDifference + 1
+
+                            }
+
+
+                        } else {
+                            yearDifference = parseInt(yearDifference);
+                            monthDifference = monthDifference;
+
+                        }
+                        if (monthDifferenceb % 2 != 0) {
+                            monthDifference = parseInt(monthDifference)
+                            dayDifference = dayDifference + 15
+
+                        } else {
+                            monthDifference = parseInt(monthDifference)
+                            dayDifference = dayDifference
+                        }
+                        if (dayDifferenceb % 2 != 0) {
+                            dayDifference = parseInt(dayDifference);
+                        }
+
+
+                    }
+                    console.log(yearDifference);
+                    console.log(monthDifference);
+                    console.log(dayDifference);
+
+
+
+
+
+
+                    if (dayDifference < 0) {
+                        dayDifference += 30;
+                        monthDifference -= 1;
+                    }
+
+                    if (monthDifference < 0) {
+                        monthDifference += 12;
+                        yearDifference -= 1;
+                    }
+                    // console.log(dayDifference);
+                    // console.log(monthDifference);
+                    // console.log(yearDifference);
+                    var all = yearDifference + '-' + monthDifference + '-' + dayDifference;
+
+
+
+
+
+
+
+
+                    // var evaluate = enddate - startdate;
+
+                    // var diffInDays = Math.floor(evaluate / (1000 * 60 * 60 * 24));
+                    // var multiply = diffInDays * selectedValue;
+
+                    // var year = parseInt(multiply / 365)
+                    // var diff = multiply % 365;
+
+                    // var month = parseInt(diff / 30)
+                    // var diffday = parseInt(diff - (month * 30))
+
+                    // var all = year + '-' + month + '-' + diffday;
+
+                    totalYear += yearDifference;
+                    totalMonth += monthDifference;
+                    totalDay += dayDifference;
+
+                    if (totalDay > 30) {
+                        totalMonth = totalMonth + 1;
+                        totalDay = totalDay - 30;
+                    }
+                    if (totalMonth > 12) {
+                        totalYear = totalYear + 1;
+                        totalMonth = totalMonth - 12;
+                    }
+
+
+                    $(this).closest('tr').find('#add').text(all);
+                });
+
                 var total = totalYear + '-' + totalMonth + '-' + totalDay;
                 $('#total-year').text(total);
             });
         });
     </script>
+
 @endsection
